586ad929f389350d3dc5c79c4650e924020f4aa7
Michael Helzer <100240057+Helzer1@users.noreply.github.com>
2024-04-09 22:23:49 -0500

    Add files via upload

---
+++ AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  mobile-app-final
+//
+//  Created by Michael Helzer on 12/14/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
